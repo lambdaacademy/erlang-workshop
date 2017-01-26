adbc1bd43d27a32030f4cc204fcb3a94affb3fd5
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2017-01-26 14:34:58 +0100

    Add documentation
    This commit adds EDoc documentation to the abacus module.

--- abacus/src/abacus.erl
+++ abacus/src/abacus.erl
@@ -1,3 +1,12 @@
+%% @author Szymon Mentel <szymon.mentel@erlang-solutions.com>
+%%
+%% @doc This is the Abacus API module.
+%%
+%% It provides basic mathematical operations:
+%% <li>addition,</li>
+%% <li>subtraction,</li>
+%% <li>multiplication,</li>
+%% <li>division.</li>
 -module(abacus).
 
 %% API exports
@@ -10,15 +19,27 @@
 %% API functions
 %%====================================================================
 
+%% @doc Adds two integers
+%%
+%% This adds `X' to `Y'.
 addition(X, Y) ->
     X+Y.
 
+%% @doc Subtracts two integers
+%%
+%% This subtracts `Y' from `X'.
 subtraction(X, Y) ->
     X-Y.
 
+%% @doc Multiplies two integers
+%%
+%% This multiplies `X' `Y' times.
 multiplication(X, Y) ->
     X*Y.
 
+%% @doc Divides two integers
+%%
+%% This multiplies divides `X' by `Y'.
 division(X, Y) ->
     X div Y.
 
